89b0ac5cb81c32169a18dc4345fc66eb66a37aa5
bernardesraischruama <bernardesraichruama@gmail.com>
2024-02-23 11:47:30 -0300

    vueeeeeeeeee

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,28 @@
 
   const contador = ref(0)
 
+  function incrementar() {
+    contador.value++;
+  }
+
   function decrementar() {
-    contador.value--
+    if (contador.value > 0) contador.value--
+  }
+
+  function recomeçar() {
+    contador.value = 0
   }
 </script>
 
 <template>
-  <button @click="contador++" >+</button>
+  <div>
+  <button @click="incrementar" >+</button>
   <button @click="decrementar">-</button>
+  <button @click="recomeçar">recomeçar</button>
   <p>Valor do Contador: {{ contador }}</p>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
